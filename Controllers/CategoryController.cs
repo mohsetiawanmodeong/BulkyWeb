d873ce00e1befbf9dfecdfe37743a907e9ecd699
mohsetiawanmodeong <mohsetiawanmodeong@gmail.com>
2024-01-12 14:03:51 +0900

    First commit

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,41 @@
+﻿using BulkyWeb.Controllers.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;   
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            /*if (obj.Name == obj.DisplayOrder.ToString()) //Category Name tidak boleh sama dengan Display Order
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }*/
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+        [DisplayName("Display Order")] //menampilkan langsung pada UI tanpa memberikan label pada index
+        [Range(1,100,ErrorMessage ="Display Order must be between 1-100")] //custom error message
+        public int DisplayOrder { get; set; }
+    }
+}
