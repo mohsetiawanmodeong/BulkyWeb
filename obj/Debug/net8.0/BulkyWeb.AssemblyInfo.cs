3fdb8643d06c4e02a6c214b85eb7deb6985b0913
mohsetiawanmodeong <mohsetiawanmodeong@gmail.com>
2024-01-12 14:12:50 +0900

    Add README.md

--- obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
+++ obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac10d6abae4b2f751c208323632f5d47992cf545")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d873ce00e1befbf9dfecdfe37743a907e9ecd699")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
